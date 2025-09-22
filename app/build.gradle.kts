b248a845e1279fdd8f494327a8dee2b2be0d8a10
Josh Hawkins <joshua.hawkins@gmail.com>
2025-09-22 12:38:15 -0600

    Update targetSdk version to 36
    Set minimum sdk version to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,11 +12,10 @@ android {
 
     defaultConfig {
         applicationId = "net.hawkins.gamescore"
-        minSdk = 26
-        targetSdk = 35
+        minSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
