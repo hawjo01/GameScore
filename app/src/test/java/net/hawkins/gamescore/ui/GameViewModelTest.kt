10dd74ad93246cbb57df65b1aa97586d3dd734a9
Josh Hawkins <joshua.hawkins@gmail.com>
2025-10-20 16:43:06 -0600

    Use static assert methods statically

--- app/src/test/java/net/hawkins/gamescore/ui/GameViewModelTest.kt
+++ app/src/test/java/net/hawkins/gamescore/ui/GameViewModelTest.kt
@@ -2,7 +2,7 @@ package net.hawkins.gamescore.ui
 
 import net.hawkins.gamescore.data.Player
 import net.hawkins.gamescore.game.GameType
-import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Test
 
 class GameViewModelTest {
@@ -10,23 +10,23 @@ class GameViewModelTest {
     @Test
     fun construction() {
         val gameViewModel = GameViewModel()
-        Assert.assertEquals(0, gameViewModel.getPlayers().size)
+        assertEquals(0, gameViewModel.getPlayers().size)
         // 2500 is used the most often so it should be first.
-        Assert.assertEquals("2500", gameViewModel.getGameType().getName())
+        assertEquals("2500", gameViewModel.getGameType().getName())
         // Currently only 2 game types are supported
-        Assert.assertEquals(2, gameViewModel.gameTypes.size)
+        assertEquals(2, gameViewModel.gameTypes.size)
     }
 
 
     @Test
     fun getGameType() {
         val gameViewModel = GameViewModel()
-        Assert.assertEquals("2500", gameViewModel.getGameType().getName())
+        assertEquals("2500", gameViewModel.getGameType().getName())
 
         val notTwentyFiveHundred =
             gameViewModel.gameTypes.first { it.getName() != "2500" }
         gameViewModel.setGameType(notTwentyFiveHundred)
-        Assert.assertEquals(notTwentyFiveHundred, gameViewModel.getGameType())
+        assertEquals(notTwentyFiveHundred, gameViewModel.getGameType())
     }
 
     @Test
@@ -34,12 +34,12 @@ class GameViewModelTest {
         val gameViewModel = GameViewModel()
         val expectedGameType = gameViewModel.gameTypes[1]
         gameViewModel.setGameType(expectedGameType)
-        Assert.assertEquals(expectedGameType, gameViewModel.getGameType())
+        assertEquals(expectedGameType, gameViewModel.getGameType())
 
         // Should do nothing because no players exist.
-        Assert.assertEquals(0, gameViewModel.getPlayers().size)
+        assertEquals(0, gameViewModel.getPlayers().size)
         gameViewModel.resetGame()
-        Assert.assertEquals(0, gameViewModel.getPlayers().size)
+        assertEquals(0, gameViewModel.getPlayers().size)
 
         gameViewModel.setPlayers(listOf("Sheldon", "Penny"))
         gameViewModel.getPlayers()[0].addScore(20)
@@ -48,24 +48,24 @@ class GameViewModelTest {
         gameViewModel.getPlayers()[1].addScore(100)
         gameViewModel.getPlayers()[1].addScore(200)
 
-        Assert.assertEquals("Sheldon", gameViewModel.getPlayers()[0].name)
-        Assert.assertEquals(listOf(20, 50), gameViewModel.getPlayers()[0].scores)
-        Assert.assertEquals(70, gameViewModel.getPlayers()[0].totalScore())
+        assertEquals("Sheldon", gameViewModel.getPlayers()[0].name)
+        assertEquals(listOf(20, 50), gameViewModel.getPlayers()[0].scores)
+        assertEquals(70, gameViewModel.getPlayers()[0].totalScore())
 
-        Assert.assertEquals("Penny", gameViewModel.getPlayers()[1].name)
-        Assert.assertEquals(listOf(100, 200), gameViewModel.getPlayers()[1].scores)
-        Assert.assertEquals(300, gameViewModel.getPlayers()[1].totalScore())
+        assertEquals("Penny", gameViewModel.getPlayers()[1].name)
+        assertEquals(listOf(100, 200), gameViewModel.getPlayers()[1].scores)
+        assertEquals(300, gameViewModel.getPlayers()[1].totalScore())
 
         gameViewModel.resetGame()
-        Assert.assertEquals(2, gameViewModel.getPlayers().size)
-        Assert.assertEquals("Sheldon", gameViewModel.getPlayers()[0].name)
-        Assert.assertEquals(listOf<Int>(), gameViewModel.getPlayers()[0].scores)
-        Assert.assertEquals(0, gameViewModel.getPlayers()[0].totalScore())
+        assertEquals(2, gameViewModel.getPlayers().size)
+        assertEquals("Sheldon", gameViewModel.getPlayers()[0].name)
+        assertEquals(listOf<Int>(), gameViewModel.getPlayers()[0].scores)
+        assertEquals(0, gameViewModel.getPlayers()[0].totalScore())
 
-        Assert.assertEquals("Penny", gameViewModel.getPlayers()[1].name)
-        Assert.assertEquals(listOf<Int>(), gameViewModel.getPlayers()[1].scores)
-        Assert.assertEquals(0, gameViewModel.getPlayers()[1].totalScore())
-        Assert.assertEquals(expectedGameType, gameViewModel.getGameType())
+        assertEquals("Penny", gameViewModel.getPlayers()[1].name)
+        assertEquals(listOf<Int>(), gameViewModel.getPlayers()[1].scores)
+        assertEquals(0, gameViewModel.getPlayers()[1].totalScore())
+        assertEquals(expectedGameType, gameViewModel.getGameType())
     }
 
     @Test
@@ -95,29 +95,29 @@ class GameViewModelTest {
 
         val gameViewModel = GameViewModel()
         gameViewModel.setGameType(Seven())
-        Assert.assertEquals("7", gameViewModel.getGameType().getName())
-        Assert.assertFalse(gameViewModel.highlightNegativeScore())
-        Assert.assertTrue(gameViewModel.hasWinningThreshold())
+        assertEquals("7", gameViewModel.getGameType().getName())
+        assertFalse(gameViewModel.highlightNegativeScore())
+        assertTrue(gameViewModel.hasWinningThreshold())
 
-        Assert.assertTrue(gameViewModel.isValidScore("7"))
-        Assert.assertFalse(gameViewModel.isValidScore("1"))
+        assertTrue(gameViewModel.isValidScore("7"))
+        assertFalse(gameViewModel.isValidScore("1"))
         gameViewModel.setPlayers(listOf("Penny", "Bernadette"))
         gameViewModel.getPlayers()[0].addScore(7)
 
         gameViewModel.getPlayers()[1].addScore(14)
 
-        Assert.assertNull(gameViewModel.determineWinner())
+        assertNull(gameViewModel.determineWinner())
 
         gameViewModel.getPlayers()[0].addScore(70)
         gameViewModel.getPlayers()[1].addScore(21)
 
         val winner = gameViewModel.determineWinner()
-        Assert.assertNotNull(winner)
-        Assert.assertEquals("Penny", winner?.name)
-        Assert.assertNotNull(gameViewModel.getWinner())
-        Assert.assertEquals("Penny", gameViewModel.getWinner()?.name)
+        assertNotNull(winner)
+        assertEquals("Penny", winner?.name)
+        assertNotNull(gameViewModel.getWinner())
+        assertEquals("Penny", gameViewModel.getWinner()?.name)
 
         gameViewModel.resetGame()
-        Assert.assertNull(gameViewModel.getWinner())
+        assertNull(gameViewModel.getWinner())
     }
 }
