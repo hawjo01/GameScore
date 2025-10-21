65f14a38e9c389475e2fdf542bafbed5c272be9f
Josh Hawkins <joshua.hawkins@gmail.com>
2025-10-21 16:36:39 -0600

    Use a common game card

--- app/src/main/java/net/hawkins/gamescore/ui/favorites/Games.kt
+++ app/src/main/java/net/hawkins/gamescore/ui/favorites/Games.kt
@@ -1,5 +1,6 @@
 package net.hawkins.gamescore.ui.favorites
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,8 +11,6 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -33,23 +32,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import net.hawkins.gamescore.R
+import net.hawkins.gamescore.favorites.FavoriteGame
 import net.hawkins.gamescore.favorites.FavoriteGames
 import net.hawkins.gamescore.game.Game
 import net.hawkins.gamescore.ui.component.ConfirmAction
 import net.hawkins.gamescore.ui.theme.DeleteRed
-import net.hawkins.gamescore.ui.theme.GoGreen
-import net.hawkins.gamescore.ui.theme.InfoBlue
+import net.hawkins.gamescore.ui.theme.SkyBlue
 import net.hawkins.gamescore.ui.theme.Typography
 
 @Composable
 fun FavoriteGamesCard(
     favoriteGames: FavoriteGames,
-    onStartButtonClick: (String, List<String>) -> Unit,
+    onFavoriteSelected: (FavoriteGame) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Card(
@@ -58,10 +59,8 @@ fun FavoriteGamesCard(
         ),
         modifier = modifier.padding(all = 10.dp)
     ) {
-
         val favorites = favoriteGames.getGames()
         if (favorites.isNotEmpty()) {
-            var showInfoIndex by remember { mutableIntStateOf(-1) }
             var showDeleteIndex by remember { mutableIntStateOf(-1) }
 
             favorites.forEachIndexed { index, favorite ->
@@ -73,122 +72,40 @@ fun FavoriteGamesCard(
                     Column {
                         Text(
                             text = favorite.name,
-                            style = Typography.labelMedium
+                            modifier = modifier
+                                .padding(horizontal = 20.dp, vertical = 10.dp)
+                                .clickable(
+                                    onClick = { onFavoriteSelected(favorite) }
+                                ),
+                            style = MaterialTheme.typography.labelMedium.plus(
+                                TextStyle(
+                                    color = SkyBlue,
+                                    textDecoration = TextDecoration.Underline
+                                )
+                            )
                         )
                     }
                     Column {
-                        Row {
-                            IconButton(onClick = {
-                                onStartButtonClick(favorite.game, favorite.players)
-                            }) {
-                                Icon(
-                                    Icons.Filled.PlayArrow,
-                                    tint = GoGreen,
-                                    contentDescription = "Play ${favorite.name}"
-                                )
-                            }
-
-
-                            IconButton(onClick = {
-                                showInfoIndex = index
-                            }) {
-                                Icon(
-                                    Icons.Filled.Info,
-                                    tint = InfoBlue,
-                                    contentDescription = "Info about ${favorite.name}"
-                                )
-                            }
-
-                            IconButton(onClick = { showDeleteIndex = index }) {
-                                Icon(
-                                    Icons.Filled.Delete,
-                                    tint = DeleteRed,
-                                    contentDescription = "Delete ${favorite.name}"
-                                )
-                            }
+                        IconButton(onClick = { showDeleteIndex = index }) {
+                            Icon(
+                                Icons.Filled.Delete,
+                                tint = DeleteRed,
+                                contentDescription = "Delete ${favorite.name}"
+                            )
                         }
                     }
                 }
-            }
-
-            if (showInfoIndex >= 0) {
-                Dialog(
-                    onDismissRequest = { showInfoIndex = -1 }
-                ) {
-                    Card(
-                        modifier = Modifier
-                            .padding(16.dp),
-                        shape = RoundedCornerShape(16.dp),
-                    ) {
-                        Column(
-                            horizontalAlignment = Alignment.Start,
-                            modifier = modifier.padding(start = 10.dp)
-                        ) {
-
-                            Row(
-                                horizontalArrangement = Arrangement.Center,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(top = 10.dp)
-                            ) {
-                                Text(
-                                    text = favorites[showInfoIndex].name,
-                                    style = Typography.titleMedium
-                                )
-                            }
-
-                            Row(modifier = modifier.padding(top = 20.dp)) {
-                                Text(
-                                    text = "Game",
-                                    style = Typography.labelMedium
-                                )
-                            }
-                            Row {
-                                Text(text = "" + favorites[showInfoIndex].game)
-                            }
-
-                            Row(modifier = modifier.padding(top = 10.dp)) {
-                                Text(
-                                    text = "Players",
-                                    style = Typography.labelMedium
-                                )
-                            }
-                            Row {
-                                Text(
-                                    text = favorites[showInfoIndex].players.joinToString(
-                                        separator = ", "
-                                    ) { name -> name })
-                            }
-
-
-                            Row(
-                                horizontalArrangement = Arrangement.Center,
-                                modifier = Modifier.fillMaxWidth()
-                            ) {
-                                TextButton(
-                                    onClick = {
-                                        showInfoIndex = -1
-                                    },
-                                    modifier = Modifier.padding(8.dp),
-                                ) {
-                                    Text(text = "OK")
-                                }
-                            }
-                        }
-                    }
+                if (showDeleteIndex >= 0) {
+                    ConfirmAction(
+                        title = "Delete Favorite Game",
+                        description = "Delete '" + favorites[showDeleteIndex].name + "'",
+                        onConfirmation = {
+                            favoriteGames.remove(favorites[showDeleteIndex])
+                            showDeleteIndex = -1
+                        },
+                        onDismissRequest = { showDeleteIndex = -1 },
+                    )
                 }
-            }
-
-            if (showDeleteIndex >= 0) {
-                ConfirmAction(
-                    title = "Delete Favorite Game",
-                    description = "Delete '" + favorites[showDeleteIndex].name + "'",
-                    onConfirmation = {
-                        favoriteGames.remove(favorites[showDeleteIndex])
-                        showDeleteIndex = -1
-                    },
-                    onDismissRequest = { showDeleteIndex = -1 },
-                )
             }
         } else {
             Row(
