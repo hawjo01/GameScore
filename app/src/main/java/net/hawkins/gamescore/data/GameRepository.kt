99363a468d8a2a2942351b6fa8bfb7e7211bf6e2
Josh Hawkins <joshua.hawkins@gmail.com>
2025-11-12 16:02:59 -0700

    Non-functional changes.
    Renamed some variables and cleaned-up some newlines.

--- app/src/main/java/net/hawkins/gamescore/data/GameRepository.kt
+++ app/src/main/java/net/hawkins/gamescore/data/GameRepository.kt
@@ -4,37 +4,39 @@ import net.hawkins.gamescore.data.model.Game
 
 class GameRepository {
 
-    fun getAll() : List<Game> {
-        return listOf(
-            Game(
-                name = "Basic Scoring - High",
-                constraints = Game.Constraints(
-                ),
-                objective = Game.Objective(
-                    type = Game.Objective.Type.HIGH_SCORE,
-                ),
-                color = Game.Colors()
+    private val games = listOf(
+        Game(
+            name = "Basic Scoring - High",
+            constraints = Game.Constraints(
             ),
-            Game(
-                name = "2500",
-                constraints = Game.Constraints(
-                    multipleOf = 5,
-                    equalHandSizes = true
-                ),
-                objective = Game.Objective(
-                    goal = 2500
-                ),
-                color = Game.Colors(
-                    negativeScore = Game.Colors.Color.RED
-                )
+            objective = Game.Objective(
+                type = Game.Objective.Type.HIGH_SCORE,
             ),
-            Game(
-                name = "Basic Scoring - Low",
-                objective = Game.Objective(
-                    type = Game.Objective.Type.LOW_SCORE
-                ),
+            color = Game.Colors()
+        ),
+        Game(
+            name = "2500",
+            constraints = Game.Constraints(
+                multipleOf = 5,
+                equalHandSizes = true
+            ),
+            objective = Game.Objective(
+                goal = 2500
+            ),
+            color = Game.Colors(
+                negativeScore = Game.Colors.Color.RED
             )
+        ),
+        Game(
+            name = "Basic Scoring - Low",
+            objective = Game.Objective(
+                type = Game.Objective.Type.LOW_SCORE
+            ),
         )
+    )
+
+    fun getAll(): List<Game> {
+        return games
     }
 
     fun getByName(name: String): Game {
