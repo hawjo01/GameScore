60d2cf27179087c451d4a891391d5638c280a9b0
J Hawkins <127335637+hawjo01@users.noreply.github.com>
2025-12-19 16:22:11 -0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-ksp(libs.hilt.compiler)
+ksp(libs.google.dagger.hilt.compiler)
 
 }
