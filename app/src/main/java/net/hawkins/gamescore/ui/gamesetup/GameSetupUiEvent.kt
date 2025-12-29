b9b660c8780c7e9b1830cc7b277a2e4ac8d6cc3b
J Hawkins <127335637+hawjo01@users.noreply.github.com>
2025-12-29 10:43:04 -0700

    Use a UiEvent for most callbacks to the ViewModel (#134)

---
+++ app/src/main/java/net/hawkins/gamescore/ui/gamesetup/GameSetupUiEvent.kt
@@ -0,0 +1,17 @@
+package net.hawkins.gamescore.ui.gamesetup
+
+import androidx.compose.runtime.Stable
+import net.hawkins.gamescore.data.model.Game
+
+@Stable
+sealed interface GameSetupUiEvent {
+    data class SetGameName(val name: String) : GameSetupUiEvent
+    data class SetConstraintPostiveOnlyScores(val positiveOnly: Boolean) : GameSetupUiEvent
+    data class SetConstraintEqualHandSizes(val requireEqualHandSizes: Boolean) : GameSetupUiEvent
+    data class SetConstraintScoreModulus(val modulus: Int?) : GameSetupUiEvent
+    data class SetObjectiveGoal(val goal: Int?) : GameSetupUiEvent
+    data class SetObjectiveType(val type: Game.Objective.Type) : GameSetupUiEvent
+    data class SetDisplayNegativeColor(val color: Game.Colors.Color) : GameSetupUiEvent
+    data class SetDisplayPositiveColor(val color: Game.Colors.Color) : GameSetupUiEvent
+    data class SetScreenMode(val mode: GameSetupUiState.Mode) : GameSetupUiEvent
+}
--- app/src/main/java/net/hawkins/gamescore/ui/gamesetup/GameSetupViewModel.kt
+++ app/src/main/java/net/hawkins/gamescore/ui/gamesetup/GameSetupViewModel.kt
@@ -9,7 +9,6 @@ import net.hawkins.gamescore.data.GameRepository
 import net.hawkins.gamescore.data.model.Game
 import net.hawkins.gamescore.ui.AbstractViewModel
 import javax.inject.Inject
-import kotlin.String
 
 @HiltViewModel
 class GameSetupViewModel @Inject constructor(
@@ -23,7 +22,21 @@ class GameSetupViewModel @Inject constructor(
         return _gameRepository.save(_uiState.value.game)
     }
 
-    fun setGameName(newName: String) {
+    fun onEvent(event: GameSetupUiEvent) {
+        when (event) {
+            is GameSetupUiEvent.SetGameName -> setGameName(event.name)
+            is GameSetupUiEvent.SetConstraintPostiveOnlyScores -> setConstraintAllowNegative(event.positiveOnly)
+            is GameSetupUiEvent.SetConstraintEqualHandSizes -> setConstraintEqualHandSizes(event.requireEqualHandSizes)
+            is GameSetupUiEvent.SetConstraintScoreModulus -> setConstraintModulus(event.modulus)
+            is GameSetupUiEvent.SetObjectiveGoal -> setObjectiveGoal(event.goal)
+            is GameSetupUiEvent.SetObjectiveType -> setObjectiveType(event.type)
+            is GameSetupUiEvent.SetDisplayNegativeColor -> setDisplayNegative(event.color)
+            is GameSetupUiEvent.SetDisplayPositiveColor -> setDisplayPositive(event.color)
+            is GameSetupUiEvent.SetScreenMode -> setMode(event.mode)
+        }
+    }
+
+    private fun setGameName(newName: String) {
         val currentGame = _uiState.value.game
         val newGame = currentGame.copy(name = newName)
         _uiState.update { currentState ->
@@ -33,7 +46,7 @@ class GameSetupViewModel @Inject constructor(
         }
     }
 
-    fun setConstraintAllowNegative(state: Boolean) {
+    private fun setConstraintAllowNegative(state: Boolean) {
         val currentGame = _uiState.value.game
         val currentConstraints = currentGame.constraints
         val newConstraints = currentConstraints.copy(positiveOnly = state)
@@ -45,7 +58,7 @@ class GameSetupViewModel @Inject constructor(
         }
     }
 
-    fun setConstraintEqualHandSizes(state: Boolean) {
+    private fun setConstraintEqualHandSizes(state: Boolean) {
         val currentGame = _uiState.value.game
         val currentConstraints = currentGame.constraints
         val newConstraints = currentConstraints.copy(equalHandSizes = state)
@@ -57,31 +70,7 @@ class GameSetupViewModel @Inject constructor(
         }
     }
 
-    fun setObjectiveType(newType: Game.Objective.Type) {
-        val currentGame = _uiState.value.game
-        val currentObjective = currentGame.objective
-        val newObjective = currentObjective.copy(type = newType)
-        val newGame = currentGame.copy(objective = newObjective)
-        _uiState.update { currentState ->
-            currentState.copy(
-                game = newGame
-            )
-        }
-    }
-
-    fun setObjectiveGoal(newGoal: Int?) {
-        val currentGame = _uiState.value.game
-        val currentObjective = currentGame.objective
-        val newObjective = currentObjective.copy(goal = newGoal)
-        val newGame = currentGame.copy(objective = newObjective)
-        _uiState.update { currentState ->
-            currentState.copy(
-                game = newGame
-            )
-        }
-    }
-
-    fun setConstraintModulus(newMultipleOf: Int?) {
+    private fun setConstraintModulus(newMultipleOf: Int?) {
         val currentGame = _uiState.value.game
         val currentConstraints = currentGame.constraints
         val newConstraints = currentConstraints.copy(multipleOf = newMultipleOf)
@@ -93,7 +82,32 @@ class GameSetupViewModel @Inject constructor(
         }
     }
 
-    fun setDisplayNegative(newColor: Game.Colors.Color) {
+    private fun setObjectiveType(newType: Game.Objective.Type) {
+        val currentGame = _uiState.value.game
+        val currentObjective = currentGame.objective
+        val newObjective = currentObjective.copy(type = newType)
+        val newGame = currentGame.copy(objective = newObjective)
+        _uiState.update { currentState ->
+            currentState.copy(
+                game = newGame
+            )
+        }
+    }
+
+    private fun setObjectiveGoal(newGoal: Int?) {
+        val currentGame = _uiState.value.game
+        val currentObjective = currentGame.objective
+        val newObjective = currentObjective.copy(goal = newGoal)
+        val newGame = currentGame.copy(objective = newObjective)
+        _uiState.update { currentState ->
+            currentState.copy(
+                game = newGame
+            )
+        }
+    }
+
+
+    private fun setDisplayNegative(newColor: Game.Colors.Color) {
         val currentGame = _uiState.value.game
         val currentColors = currentGame.color
         val newColors = currentColors.copy(negativeScore = newColor)
@@ -105,7 +119,7 @@ class GameSetupViewModel @Inject constructor(
         }
     }
 
-    fun setDisplayPositive(newColor: Game.Colors.Color) {
+    private fun setDisplayPositive(newColor: Game.Colors.Color) {
         val currentGame = _uiState.value.game
         val currentColors = currentGame.color
         val newColors = currentColors.copy(positiveScore = newColor)
@@ -125,7 +139,7 @@ class GameSetupViewModel @Inject constructor(
         }
     }
 
-    fun setMode(newMode:  GameSetupUiState.Mode) {
+    private fun setMode(newMode: GameSetupUiState.Mode) {
         _uiState.update { currentState ->
             currentState.copy(
                 mode = newMode
