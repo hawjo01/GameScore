7e6d9718c6bebc9bd6601938734bf05e50c0b3ad
Josh Hawkins <joshua.hawkins@gmail.com>
2026-01-21 17:12:27 -0700

    Upgrade hilt-dagger and complete migration to AGP 9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -47,11 +45,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlin {
-        compilerOptions {
-            jvmTarget = JvmTarget.JVM_17
-        }
     }
     buildFeatures {
         compose = true
