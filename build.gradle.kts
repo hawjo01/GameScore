6f14da3aa8c7cc832ec189223fd88bc6b3738605
Josh Hawkins <joshua.hawkins@gmail.com>
2026-02-16 09:58:59 -0700

    Force upgrade of transitive test dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,12 @@ plugins {
     alias(libs.plugins.google.dagger.hilt) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
 }
+
+configurations.all {
+    resolutionStrategy {
+        // Because CVE-2024-7254
+        force("com.google.protobuf:protobuf-kotlin:3.25.5")
+        // Because CVE-2024-7254
+        force("com.google.protobug:protobuf-java:3.25.5")
+    }
+}
