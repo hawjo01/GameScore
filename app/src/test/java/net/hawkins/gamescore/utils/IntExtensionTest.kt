98850b2014fe07728bcc776d17eb8a629a840f28
J Hawkins <127335637+hawjo01@users.noreply.github.com>
2026-03-03 11:30:10 -0500

    Add test (#236)

--- app/src/test/java/net/hawkins/gamescore/utils/IntExtensionTest.kt
+++ app/src/test/java/net/hawkins/gamescore/utils/IntExtensionTest.kt
@@ -8,10 +8,19 @@ class IntExtensionTest {
 
     @Test
     fun isNegativeInt() {
-        val negative1 = -1
-        assertTrue(negative1.isNegative())
+        assertTrue((-1).isNegative())
 
         assertFalse(0.isNegative())
         assertFalse(1.isNegative())
+    }
+
+    @Test
+    fun isEven() {
+        assertTrue((-2).isEven())
+        assertTrue(0.isEven())
+        assertTrue(2.isEven())
+
+        assertFalse(1.isEven())
+        assertFalse((-1).isEven())
     }
 }
