f609f2ba4931aa7d1acb903678c179ef75b1be8b
J Hawkins <127335637+hawjo01@users.noreply.github.com>
2026-07-04 15:46:53 -0600

    Update plugin transitive dependencies. (#288)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
+buildscript {
+    dependencies {
+        // Override plugin transitive dependencies to resolve Dependabot alerts
+        classpath("org.apache.commons:commons-lang3:3.18.0")
+        classpath("org.bitbucket.b_c:jose4j:0.9.6")
+        classpath("org.bouncycastle:bcpkix-jdk18on:1.84")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
