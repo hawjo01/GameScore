554e10a8cf2a7ab9123d8b2891d453c3f085ebe9
Josh Hawkins <joshua.hawkins@gmail.com>
2026-07-15 07:30:31 -0600

    Enforce plugin dependency update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,29 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+    configurations.all {
+        resolutionStrategy {
+            force("org.apache.commons:commons-lang3:3.18.0")
+            force("org.bitbucket.b_c:jose4j:0.9.6")
+            force("org.bouncycastle:bcpkix-jdk18on:1.85")
+            force("org.bouncycastle:bcprov-jdk18on:1.85")
+        }
+    }
+    dependencies {
+        // Override plugin transitive dependencies to resolve Dependabot alerts
+        classpath("org.apache.commons:commons-lang3:3.18.0")
+        classpath("org.bitbucket.b_c:jose4j:0.9.6")
+        classpath("org.bouncycastle:bcpkix-jdk18on:1.85")
+        classpath("org.bouncycastle:bcprov-jdk18on:1.85")
+    }
+}
+
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
