54865095bd3af70d9811a482c200e77e27c6b553
Josh Hawkins <joshua.hawkins@gmail.com>
2026-07-15 07:30:31 -0600

    Enforce plugin dependency update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,18 @@
 buildscript {
+    configurations.all {
+        resolutionStrategy {
+            force("org.apache.commons:commons-lang3:3.18.0")
+            force("org.bitbucket.b_c:jose4j:0.9.6")
+            force("org.bouncycastle:bcpkix-jdk18on:1.85")
+            force("org.bouncycastle:bcprov-jdk18on:1.85")
+        }
+    }
     dependencies {
         // Override plugin transitive dependencies to resolve Dependabot alerts
         classpath("org.apache.commons:commons-lang3:3.18.0")
         classpath("org.bitbucket.b_c:jose4j:0.9.6")
         classpath("org.bouncycastle:bcpkix-jdk18on:1.85")
+        classpath("org.bouncycastle:bcprov-jdk18on:1.85")
     }
 }
 
