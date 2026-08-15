c120d582f5d067b575b98fc3f69e265eb8818961
J Hawkins <127335637+hawjo01@users.noreply.github.com>
2026-08-15 09:01:35 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
         // Override plugin transitive dependencies to resolve Dependabot alerts
         classpath("org.apache.commons:commons-lang3:3.18.0")
         classpath("org.bitbucket.b_c:jose4j:0.9.6")
-        classpath("org.bouncycastle:bcpkix-jdk18on:1.85.2")
+        classpath("org.bouncycastle:bcpkix-jdk18on:1.85")
         classpath("org.bouncycastle:bcprov-jdk18on:1.85.2")
     }
 }
